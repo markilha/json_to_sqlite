0848b0d287e37b3ff5c95ee2a9101bc1b5c66322
markilha <markilha@gmail.com>
2023-07-11 11:44:29 -0300

    Subiu Projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,131 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [json, setJson] = useState(null);
+  const [sql, setSql] = useState("");
+  const [nameTable, setNameTable] = useState("");
+
+  function converterJsonEmSql(json) {
+    // Verificar se o json é válido
+    if (json && typeof json === "object") {
+      // Inicializar uma variável para armazenar o sql
+      let sql = "";
+      // Iterar sobre as chaves do json
+      for (let chave in json) {
+        // Obter o valor da chave
+        let valor = json[chave];
+
+        // Verificar se o valor é um objeto
+        if (typeof valor === "object") {
+          // Converter o valor em sql recursivamente
+          let subsql = converterJsonEmSql(valor);
+
+          // Adicionar o subsql ao sql com a chave como nome da tabela
+          sql += `CREATE TABLE ${chave} (\n${subsql}\n);\n`;
+        } else {
+          // Adicionar o valor ao sql com a chave como nome da coluna e o tipo de dado adequado
+          if (typeof valor === "string") {
+            sql += `${chave} VARCHAR(255),\n`;
+          } else if (typeof valor === "number") {
+            sql += `${chave} INT,\n`;
+          } else if (typeof valor === "boolean") {
+            sql += `${chave} BOOLEAN,\n`;
+          }
+        }
+      }
+
+      // Remover a última vírgula do sql
+      sql = sql.slice(0, -2);
+
+      // Retornar o sql
+      return sql;
+    } else {
+      // Retornar uma mensagem de erro se o json não for válido
+      return "O arquivo json não é válido.";
+    }
+  }
+  function converterJsonEmSqliteInsert(json, nomeTabela) {
+    if (json && typeof json === "object") {
+      let sqlite = "";
+      let chaves = Object.keys(json);
+      let valores = Object.values(json);
+      if (nomeTabela) {
+        sqlite += `INSERT INTO ${nomeTabela} (${chaves.join(", ")}) VALUES (\n`;
+      }
+
+      for (let valor of valores) {
+        if (typeof valor === "object") {
+          let subsqlite = converterJsonEmSqliteInsert(valor, nameTable);
+          sqlite += `${subsqlite}\n`;
+        } else {
+          if (typeof valor === "string") {
+            sqlite += `'${valor}',\n`;
+          } else {
+            sqlite += `${valor},\n`;
+          }
+        }
+      }
+      sqlite = sqlite.slice(0, -2);
+
+      if (nomeTabela) {
+        sqlite += ");\n";
+      }
+      return sqlite;
+    } else {
+      return "O arquivo json não é válido.";
+    }
+  }
+
+  function handleChange(event) {
+    let arquivo = event.target.files[0];
+
+    if (arquivo && arquivo.name.endsWith(".json")) {
+      let leitor = new FileReader();
+      leitor.onload = function (e) {
+        let conteudo = e.target.result;
+        try {
+          let json = JSON.parse(conteudo);
+          setJson(json);
+          let sql = converterJsonEmSqliteInsert(json);
+          setSql(sql);
+        } catch (erro) {
+          alert("O arquivo json não é válido.");
+        }
+      };
+      leitor.readAsText(arquivo);
+    } else {
+      alert("Por favor, selecione um arquivo .json válido.");
+    }
+  }
+
+  function handleClick() {
+    if (sql) {
+      let link = document.createElement("a");
+      link.href = URL.createObjectURL(new Blob([sql], { type: "text/plain" }));
+      link.download = "sql.sql";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      alert("Não há nada para salvar.");
+    }
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className={styles.pagina}>
+      <h1>CONVERTER JSON PARA SQLITE</h1>
+      <p>
+        Selecione um arquivo
+      </p>
+      <input type="file" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="Digite o nome da tabela"
+        onChange={setNameTable}
+      />
+      <button onClick={handleClick}>Salvar</button>
+      <pre>{sql}</pre>
+    </div>
+  );
 }
